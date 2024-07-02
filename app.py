41326f755f1aa83215e840b0f453d9cc8d33f207
Lazar Savkovic <savkoviclazar1@gmail.com>
2024-07-02 14:56:36 +0200

    new improved template with forms

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from storage import JsonStorage
 from util.util_package import get_picture
 
 app = Flask(__name__)
 
+my_storage = JsonStorage('data/tasks.json')
+
 @app.route("/")
 def homepage():
-    my_storage = JsonStorage('data/tasks.json')
     tasks = my_storage.load_tasks()
     for task in tasks:
         task['image'] = get_picture(task['task'])
-    print(tasks)
     return render_template("template.html", tasks=tasks)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
